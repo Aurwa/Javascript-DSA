ec98a78585eec8b6dbef9984df4ebe6513c32176
Aurwa <zafaraurwa11@gmail.com>
2024-06-28 15:50:07 +0500

    Problem_Solving

---
+++ 2_Problem Solving/script.js
@@ -0,0 +1,24 @@
+// Write a function which takes in a string and returns counts of each character in the string
+
+let charCount = {};
+
+function countChar(str) {
+  for (let char of str) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+  return charCount;
+}
+
+console.log(countChar("Hello world!"));
+
+// let pName = "name";
+// const obj = {
+//   name: "Aurwa",
+//   pass: 123,
+// };
+
+// console.log(obj[pName]);

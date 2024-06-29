029034540c0a30cac9796b2b336dcdf990a152a7
Aurwa <zafaraurwa11@gmail.com>
2024-06-29 17:11:24 +0500

    Update script.js

--- 2_Problem Solving/script.js
+++ 2_Problem Solving/script.js
@@ -196,8 +196,8 @@ function countChar(str) {
 
 // Practice Function 2
 
-const arrayOne = [1, 2, 3];
-const arrayTwo = [9, 1, 4];
+// const arrayOne = [1, 2, 3];
+// const arrayTwo = [9, 1, 4];
 
 // -- Method 1 -- O(n^2)
 /*
@@ -388,6 +388,8 @@ console.log(validAnagram("cinemaa", "icemann"));
 
 // 1. Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair doesn’t exist.
 
+// Method 1 (not the best) ==> O(n^2)
+/*
 function sumZero(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (j = i + 1; j < arr.length; j++) {
@@ -399,3 +401,23 @@ function sumZero(arr) {
 }
 
 console.log(sumZero([-2, 0, 1, 3]));
+*/
+
+// Method 2 ==> O(n)
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+console.log(sumZero([-3, -2, 0, 1, 2]));

666fb9be62b08386a825a988cc116d70abdb8762
Aurwa <zafaraurwa11@gmail.com>
2024-06-30 18:47:47 +0500

    Update app.js

--- 2_Problem Solving/app.js
+++ 2_Problem Solving/app.js
@@ -455,3 +455,34 @@ function areThereDuplicates(...args) {
 
 console.log(areThereDuplicates("a", "b", "c", "b"));
 */
+
+// 2. aveargePair
+
+// Write a function called averagePair. Given a sorted array of integers and a target average, determine if there is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target.
+
+// averagePair([1, 2, 3], 2.5); ==> true
+// averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8); ==> true
+
+function averagePair(arr, targetAvg) {
+  let targetSum = targetAvg * 2;
+  console.log(targetSum);
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < arr.length) {
+    let currentSum = arr[left] + arr[right];
+    if (currentSum === targetSum) {
+      return true;
+    } else if (currentSum < targetSum) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+}
+
+console.log(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8));
+// Since the array is sorted in ascending order:
+
+// Incrementing the left pointer moves to a larger value, which will increase the currentSum.
+
+// Decrementing the right pointer moves to a smaller value, which will decrease the currentSum.

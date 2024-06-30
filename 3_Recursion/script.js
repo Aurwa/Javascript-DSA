63eb4693506e73872889dc731d09eedb4c50a5c9
Aurwa <zafaraurwa11@gmail.com>
2024-07-01 00:12:50 +0500

    Update script.js

---
+++ 3_Recursion/script.js
@@ -0,0 +1,25 @@
+// Function1
+/*
+// with recursion
+function countDown(num) {
+  if (num <= 0) {
+    console.log("All done.");
+    return;
+  }
+  console.log(num);
+  num--;
+  countDown(num);
+}
+
+countDown(5);
+
+// without recursion
+// function countDown(num) {
+//   for (let i = num; i > 0; i--) {
+//     console.log(i);
+//   }
+//   console.log("All done.");
+// }
+
+// countDown(5);
+*/

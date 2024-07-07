d9b95f1eb9d3ebcd2a4f51fc8026c60324335e78
Aurwa <zafaraurwa11@gmail.com>
2024-07-07 23:46:13 +0500

    Update app.js

--- 2_Problem Solving/app.js
+++ 2_Problem Solving/app.js
@@ -650,3 +650,23 @@ function findLongestSubstring(str) {
 
 console.log(findLongestSubstring("abcabcab"));
 */
+
+// practice findLongestSubstring
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+  }
+
+  return longest;
+}
+
+console.log(findLongestSubstring("abcabcab"));
